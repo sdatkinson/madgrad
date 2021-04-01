58f71b5ddf876e538e04aa4fcf14d41f5915ff72
Steven Atkinson <steven@atkinson.mn>
2021-03-31 21:06:27 -0400

    Test examples

---
+++ tests/test_examples.py
@@ -0,0 +1,26 @@
+# File: test_examples.py
+# File Created: Wednesday, 31st March 2021 9:01:36 pm
+# Author: Steven Atkinson (steven@atkinson.mn)
+
+import logging
+import os
+import sys
+from subprocess import check_call
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+_examples_dir = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "examples")
+)
+_examples = ["basic.py"]
+
+
+@pytest.mark.parametrize("example", _examples)
+def test(example):
+    logger.info("Running:\npython examples/{}".format(example))
+    example = example.split()
+    filename, args = example[0], example[1:]
+    filename = os.path.join(_examples_dir, filename)
+    check_call([sys.executable, filename] + args)
